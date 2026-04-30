5cf4dfaa1ced4152447685d57a5dd50e3243fcee
dariabratko7 <dariabratko7@gmail.com>
2026-04-30 22:58:23 +0300

    Add files via upload

---
+++ dz2204/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Створити клас «TeachersWebinar», з властивостями: ім'я, вік та запрошення.  Створіть у класі статичне поле запрошення. На вебінар можуть потрапити
+//    учасники лише старше 25 років та із запрошенням – “VeInar2021”. 
+//    В разі невідповідності, генерувати помилку через свій клас винятків. При виведенні інформації про помилку – виводити, що не так і саме значення.
+
+//public class WebinarException : Exception
+//{
+//    public WebinarException(string message) : base(message) { }
+//}
+
+//public class TeachersWebinar
+//{
+//    public string Name { get; set; }
+//    public int Age { get; set; }
+
+//    public static string RequiredInvite = "VeInar2021";
+
+//    private string invite;
+
+//    public string Invite
+//    {
+//        get { return invite; }
+//        set
+//        {
+//            invite = value;
+//        }
+//    }
+
+//    public TeachersWebinar(string name, int age, string invite)
+//    {
+//        Name = name;
+//        Age = age;
+//        Invite = invite;
+
+//        Validate();
+//    }
+
+//    private void Validate()
+//    {
+//        if (Age <= 25)
+//        {
+//            throw new WebinarException($"Помилка: вік менше або дорівнює 25! Значення: {Age}");
+//        }
+
+//        if (Invite != RequiredInvite)
+//        {
+//            throw new WebinarException($"Помилка: неправильне запрошення! Значення: {Invite}");
+//        }
+//    }
+//}
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        try
+//        {
+//            TeachersWebinar t1 = new TeachersWebinar("Іван", 30, "VeInar2021");
+//            Console.WriteLine("Учасник допущений!");
+//        }
+//        catch (WebinarException ex)
+//        {
+//            Console.WriteLine(ex.Message);
+//        }
+
+//        try
+//        {
+//            TeachersWebinar t2 = new TeachersWebinar("Петро", 22, "VeInar2021");
+//        }
+//        catch (WebinarException ex)
+//        {
+//            Console.WriteLine(ex.Message);
+//        }
+
+//        try
+//        {
+//            TeachersWebinar t3 = new TeachersWebinar("Оля", 28, "WrongCode");
+//        }
+//        catch (WebinarException ex)
+//        {
+//            Console.WriteLine(ex.Message);
+//        }
+//    }
+//}
+
+//У газетному виданні для публікації статті є певні норми. Ваше завдання, пропускати статті, які містять
+//    щонайменше 30 пропозицій та 1000 слів. Розробте клас із властивостями: назва, текст, автор. У разі невідповідності тексту, не приймайте статтю, 
+//    повертайте помилку, з інформацією чого не вистачило (пропозицій чи слів). Для вилову помилок використовувати конструкцію try/catch.
+public class ArticleException : Exception
+{
+    public ArticleException(string message) : base(message) { }
+}
+
+public class Article
+{
+    public string Title { get; set; }
+    public string Text { get; set; }
+    public string Author { get; set; }
+
+    public Article(string title, string text, string author)
+    {
+        Title = title;
+        Text = text;
+        Author = author;
+
+        Validate();
+    }
+
+    private void Validate()
+    {
+        int sentenceCount = CountSentences(Text);
+        int wordCount = CountWords(Text);
+
+        if (sentenceCount < 30 && wordCount < 1000)
+        {
+            throw new ArticleException(
+                $"Недостатньо речень ({sentenceCount}) і слів ({wordCount})"
+            );
+        }
+        else if (sentenceCount < 30)
+        {
+            throw new ArticleException(
+                $"Недостатньо речень! Є: {sentenceCount}"
+            );
+        }
+        else if (wordCount < 1000)
+        {
+            throw new ArticleException(
+                $"Недостатньо слів! Є: {wordCount}"
+            );
+        }
+    }
+
+    private int CountSentences(string text)
+    {
+        char[] delimiters = { '.', '!', '?' };
+        return text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Length;
+    }
+
+    private int CountWords(string text)
+    {
+        return text.Split(new char[] { ' ', '\n', '\t' },
+            StringSplitOptions.RemoveEmptyEntries).Length;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            string text = "Тут має бути дуже довгий текст..."; // тест
+
+            Article article = new Article(
+                "Моя стаття",
+                text,
+                "Автор"
+            );
+
+            Console.WriteLine("Стаття прийнята!");
+        }
+        catch (ArticleException ex)
+        {
+            Console.WriteLine("Помилка: " + ex.Message);
+        }
+    }
+}
+
---
+++ dz2204/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("dz2204")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("dz2204")]
+[assembly: AssemblyCopyright("Copyright © HP 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("84151047-2980-46f2-a1c8-1024d04607fe")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

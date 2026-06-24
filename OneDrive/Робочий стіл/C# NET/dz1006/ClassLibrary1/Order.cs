f2518d63cc60126279e970f3893125b1d0514e77
dariabratko7 <dariabratko7@gmail.com>
2026-06-24 13:30:34 +0300

    додала dz1006

---
+++ OneDrive/Робочий стіл/C# NET/dz1006/ClassLibrary1/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShopLibrary
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
+        public List<Product> Products { get; set; } = new();
+    }
+}
---
+++ OneDrive/Робочий стіл/C# NET/dz1006/ClassLibrary1/ShopContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShopLibrary
+{
+    public class ShopContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ShopDb;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, Name = "Ноутбук", Price = 35000 },
+                new Product { Id = 2, Name = "Мишка", Price = 800 },
+                new Product { Id = 3, Name = "Клавіатура", Price = 1500 }
+            );
+        }
+    }
+}

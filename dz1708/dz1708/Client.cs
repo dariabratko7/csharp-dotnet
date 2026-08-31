e608d24df65f95c49e595c2c2ea6d4befd78e3b2
dariabratko7 <dariabratko7@gmail.com>
2026-08-31 19:44:27 +0300

    Add files via upload

---
+++ dz1708/dz1708/Client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+class Client
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        string serverIp = "127.0.0.1"; 
+        int serverPort = 5000;
+
+        using (UdpClient udpClient = new UdpClient())
+        {
+            udpClient.Connect(serverIp, serverPort);
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            Console.WriteLine("--- КЛІЄНТ ПОШУКУ ЦІН КОМПЛЕКТУЮЧИХ ---");
+            Console.WriteLine("Введіть назву запчастини (наприклад: процесор, відеокарта, ssd).");
+            Console.WriteLine("Для виходу введіть 'exit'.\n");
+
+            while (true)
+            {
+                Console.Write("Введіть запит: ");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                    continue;
+
+                if (input.Equals("exit", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Завершення роботи...");
+                    break;
+                }
+
+                byte[] sendBytes = Encoding.UTF8.GetBytes(input);
+                udpClient.Send(sendBytes, sendBytes.Length);
+                
+                byte[] receiveBytes = udpClient.Receive(ref remoteEndPoint);
+                string response = Encoding.UTF8.GetString(receiveBytes);
+
+                Console.WriteLine($"[ВІДПОВІДЬ СЕРВЕРА]: {response}\n");
+            }
+        }
+    }
+}
---
+++ dz1708/dz1708/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+class Server
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        int port = 5000;
+
+        var priceList = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+        {
+            { "процесор", "12 500 грн" },
+            { "відеокарта", "28 000 грн" },
+            { "оперативна пам'ять", "3 200 грн" },
+            { "материнська плата", "6 800 грн" },
+            { "ssd", "2 500 грн" },
+            { "блок живлення", "4 100 грн" }
+        };
+
+        using (UdpClient udpServer = new UdpClient(port))
+        {
+            Console.WriteLine($"[СЕРВЕР] Запущено на порту {port}. Очікування запитів...");
+
+            while (true)
+            {
+                IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                byte[] receiveBytes = udpServer.Receive(ref clientEndPoint);
+                string request = Encoding.UTF8.GetString(receiveBytes).Trim();
+
+                Console.WriteLine($"[СЕРВЕР] Отримано запит від {clientEndPoint}: '{request}'");
+
+                string response;
+                if (priceList.TryGetValue(request, out string price))
+                {
+                    response = $"Ціна на '{request}': {price}";
+                }
+                else
+                {
+                    response = $"Товар '{request}' не знайдено у прайс-листі.";
+                }
+
+                byte[] sendBytes = Encoding.UTF8.GetBytes(response);
+                udpServer.Send(sendBytes, sendBytes.Length, clientEndPoint);
+            }
+        }
+    }
+}
